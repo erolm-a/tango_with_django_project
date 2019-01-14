5c67f4020ba150b7c9bb797ff55d37aa214f0549
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-01-14 14:43:48 +0000

    adding basic templates

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    message = "Rango says hey there partner!<br>" + \
-              "Click <a href='/rango/about'>here</a> to know more about me."
-    return HttpResponse(message)
+    # construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     message = "Rango says here is the about page.<br>" + \
