e045492efac1ee9beaceb8160bba716c91b35fe9
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-01-24 22:53:09 +0000

    bcrypt won't be available during the checkpoint

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -112,8 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 PASSWORD_HASHERS = (
-        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-        'django.contrib.auth.hashers.BCryptPasswordHasher',
+        # 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+        # 'django.contrib.auth.hashers.BCryptPasswordHasher',
         'django.contrib.auth.hashers.PBKDF2PasswordHasher',
         'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 )
